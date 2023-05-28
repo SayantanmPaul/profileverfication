0502c9b15a1ee8ce11436a75fb541be79da8ce8b
SayantanStackflow <sayantan.college6291@gmail.com>
2023-05-28 20:31:39 +0530

    mobile responsive added

--- client/src/components/username.jsx
+++ client/src/components/username.jsx
@@ -34,15 +34,52 @@ export default function Username() {
                 </div>
                 <div className='flex flex-row gap-1'>
                   <p style={{fontFamily: 'Poppins, sans-serif'}} className='text-[#96B7C5] text-xs' >Don&apos;t have an account? </p>
-                  <p style={{fontFamily: 'Poppins, sans-serif'}} className='text-black text-xs hover:underline duration-300'>Sign up for free</p>
+                  <p style={{fontFamily: 'Poppins, sans-serif'}} className='text-black text-xs hover:underline duration-300 cursor-pointer'>Sign up for free</p>
                 </div>
               </div>
             </div>
           </form>
 
         </div>
-        <div className="flex-grow bg-cover bg-no-repeat " style={{backgroundImage: "url(../../public/unsplash.jpg)"}}>
+        <div className="flex-grow bg-cover bg-no-repeat backdrop-blur-sm flex justify-center items-center" style={{backgroundImage: "url(../../public/unsplash.jpg)"}}>
+        {/* mobile and tablet view */}
+        
+          <div className='w-[92%] h-2/3 bg-white flex flex-col justify-center items-center lg:hidden'>
+
+            <div className='flex flex-row items-center hover:underline underline-offset-2 duration-300 p-5 absolute top-0'>
+              <img src={git} alt="branch" className='w-[12px]' />
+              <div>
+                <h1 className=' text-xs text-white opacity-60 '>/Project UX..</h1>
+              </div>
+            </div>
+            <form className='flex justify-center items-center h-full'>
+              <div className=' flex flex-col gap-9 '>
+                <div className=' flex  flex-col gap-1'>
+                  <h1 className=' text-[20px] font-semibold text-center' style={{fontFamily: 'Poppins, sans-serif'}}>Welcome back, User</h1>
+                  <p className=' text-[10px] font-medium text-center  text-[#96B7C5]' style={{fontFamily: 'Poppins, sans-serif'}}>Welcome back! Please enter your details.</p>
+                </div>
+                <div className=' flex flex-col items-center'>
+                  <div className=' rounded-full border-2 border-[#439BC0] p-1 '>
+                    <div className=' w-28 h-28 rounded-full overflow-hidden ' >
+                      <img className=' object-cover object-center ' src={Context} alt="context"  />
+                    </div>
+                  </div>
+                </div>
+                <div className='flex flex-col items-center gap-3'>
+                  <input className=' w-[310px] p-2 text-[12px] bg-[#e6e2e2] placeholder:opacity-70 rounded-sm' style={{fontFamily: 'Poppins, sans-serif'}} type="text" placeholder='Enter your username' />
+                  <div >
+                    <button className='bg-[#439BC0] px-[115px] py-1 text-white font-medium rounded-sm hover:bg-[#3a87a8] duration-300 text-[14px] ' style={{fontFamily: 'Poppins, sans-serif'}}>Let&apos;s sign in</button>
+                  </div>
+                  <div className='flex flex-row gap-1'>
+                    <p style={{fontFamily: 'Poppins, sans-serif'}} className='text-[#96B7C5] text-[9px]' >Don&apos;t have an account? </p>
+                    <p style={{fontFamily: 'Poppins, sans-serif'}} className='text-black text-[9px] hover:underline duration-300 cursor-pointer'>Sign up for free</p>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
+        </div>
+
       </div>
     </div>
   );
