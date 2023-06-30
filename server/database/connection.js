8f021f18b3d2bde55d42bb056e3516c50eb07d9b
SayantanStackflow <sayantan.college6291@gmail.com>
2023-06-30 13:02:31 +0530

    user registration mogo dbs

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,9 +3,9 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 
 async function connect(){
     const mongodb=await MongoMemoryServer.create();
-    const getURI=mongodb.getUri();
+    const getURI=mongodb.getUri(); //retrive mogodb connection from memeory server
 
-    mongoose.set('strictQuery', true)
+    mongoose.set('strictQuery', true) //handle undefined query
     const db=await mongoose.connect(getURI);
     console.log("database is connected ");
     return db;
---
+++ server/model/user.model.js
@@ -0,0 +1,27 @@
+import * as mongoose from 'mongoose'
+
+export const UserSchema= new mongoose.Schema({
+    // defineing the user schema
+    username:{
+        type: String,
+        required: [true, "please provide unique username"],
+        unique: [true, 'username exists!']
+    },
+    password:{
+        type: String,
+        required: [true, 'please provide a password'],
+        unique: false
+    },
+    email:{
+        type: String,
+        required: [true, 'please provide a email address'],
+        unique: true
+    },
+    
+    firstname: { type: String},
+    lastname: { type: String},
+    githubid: { type: String}
+
+});
+
+export default mongoose.model('User', UserSchema);
