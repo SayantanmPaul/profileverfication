4298436641b85a125dc5583a78ece8f8820455d6
SayantanStackflow <sayantan.college6291@gmail.com>
2023-07-06 23:18:51 +0530

    mongoose server delete

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,9 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';    
-import { MongoMemoryServer } from "mongodb-memory-server";
 
 dotenv.config({path: './config.env'})
 
 async function connect(){
-
-    const mongod = await MongoMemoryServer.create();
-    const getUri = mongod.getUri();
 
     mongoose.set('strictQuery', true)
     // const db = await mongoose.connect(getUri);
